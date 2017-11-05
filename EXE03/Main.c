868e27d38357bed9c22551b7c7d136cb08a2c9f5
BryanCruz <bryan.bialokur@gmail.com>
2017-11-05 15:08:55 -0200

    modificacao na estrutura dos testes

--- EXE03/Main.c
+++ EXE03/Main.c
@@ -122,8 +122,29 @@ int cruza(segmento s, segmento t){
 
 /*  Retorna 1 se o ponto p est ́a no interior do tri^angulo t.
 Devolve 0 caso contr ́ario. */
-int dentro(ponto p, triangulo t){
-  return (sentido(p, t.p, t.q) == sentido(p, t.q, t.r)) && (sentido(p, t.q, t.r) == sentido(p, t.r, t.p));
+int dentro (ponto p, triangulo t){
+	int aux = 0;
+	double v1, v2, v3, v4, v5, v6;
+
+	v1 = produto_vetorial(t.r - t.q, p - t.p);
+	v2 = produto_vetorial(t.r - t.q, t.p - t.q);
+	v3 = produto_vetorial(t.r - t.p, p - t.p);
+  v4 = produto_vetorial(t.r - t.p, t.q - t.p);
+  v5 = produto_vetorial(t.q - t.p, p - t.p);
+	v6 = produto_vetorial(t.q - t.p, t.r - p);
+
+	if(produto_interno(v1, v2) >= 0 && produto_interno(v3, v4)>=0 && produto_interno(v5, v6)>=0){
+		aux = 1;
+	}
+
+	if(aux == 1){
+		return 1;
+	}
+    else{
+		return 0;
+	}
+
+
 }
 
 //  Opcional:
